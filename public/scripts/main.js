c7d37549b97d731d3482e54d1e589d7d705d9734
kigari94 <xxchillstylexx@googlemail.com>
2019-10-21 22:33:23 +0200

    adding basic upload functionality

---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+// https://www.w3schools.com/html/html5_draganddrop.asp
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
